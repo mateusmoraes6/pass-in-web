26410b57e79ca685ce3059aab0c5be10f2ed7871
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-04-08 10:56:22 -0300

    Done

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
 import { AttendeeList } from "./components/attendee-list";
 import { Header } from "./components/header";
 
-
 export function App() {
   return (
     <div className="max-w-[1216px] mx-auto py-5">
@@ -10,4 +9,3 @@ export function App() {
     </div>
   )
 }
-
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,15 @@
-import icon from '../assets/icon.svg'
+import nlwUniteIcon from '../assets/icon.svg';
+import { NavLink } from './nav-link';
 
 export function Header() {
-    return (
-        <div className='flex items-center gap-5'>
-            <img src={icon} />
+  return (
+    <div className="flex items-center gap-5 py-2">
+      <img src={nlwUniteIcon} alt="NLW Unite" />
 
-            <nav className='flex items-center gap-5'>
-                <a href="#" className='font-medium text-sm text-zinc-300'>Eventos</a>
-                <a href="#" className='font-medium text-sm'>Participantes</a>
-            </nav>
-        </div>
-    )
+      <nav className="flex items-center gap-5">
+        <NavLink href="/eventos">Eventos</NavLink>
+        <NavLink href="/participantes">Participantes</NavLink>
+      </nav>
+    </div>
+  );
 }
